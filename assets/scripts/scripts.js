ce86f40e1e194a144093c614d76671b1b9885b86
MagneticShade <senya.krivorot11@gmail.com>
2023-09-30 20:28:10 +0200

    fetch test 2

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -10,10 +10,12 @@ Telegram.WebApp.onEvent('themeChanged', function() {
 Telegram.WebApp.MainButton.setParams({
     text: 'Main Button'
 });
+console.log(Telegram.WebApp.initData)
 fetch("http://localhost:8080",{
     method:'POST',
     body:`${Telegram.WebApp.initData}`
 }).then(response=>{
+    console.log(response)
     return response.json()
 }).then(result=>{
     console.log(result)
